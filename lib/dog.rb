b5314fc256b3132fbfd76ed85d8e0440a78e7721
Cara Morelli <caramorelli7@gmail.com>
2018-10-09 14:05:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+
+
+class Dog 
+
+  
+  def initialize(file, name)
+    @name = name
+  end 
+  
+  def name 
+    "#{name}"
+  end 
+  
+  
+end 
+
+   fido = Dog.new 
+    snoopy = Dog.new
+    lassie = Dog.new
